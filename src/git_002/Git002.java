4edf468b4de45a5222e117836cd4d8ee0e260609
yalcin1453 <69158410+yalcin1453@users.noreply.github.com>
2020-08-09 11:39:20 -0400

    Update Git002.java

--- src/git_002/Git002.java
+++ src/git_002/Git002.java
@@ -7,6 +7,7 @@ public class Git002 {
 		System.out.println("this is the first changes from local master");
 		System.out.println("this is second commit from local master");
 		System.out.println("this is third commit from local master");
+		System.out.println("this is third commit from local master");
 
 	}
 
