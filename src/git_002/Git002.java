e65a077192d6dcfa2b4a898a5e9e7febea2fd103
yalcin1453 <69158410+yalcin1453@users.noreply.github.com>
2020-08-09 11:53:52 -0400

    Update Git002.java
    the second commit from remote master

--- src/git_002/Git002.java
+++ src/git_002/Git002.java
@@ -9,6 +9,10 @@ public class Git002 {
 		System.out.println("this is third commit from local master");
 		System.out.println("this is the first changes from local master");
 		System.out.println("another change ");
+		
+		
+		
+		System.out.println("this is second  commit from local master");
 
 	}
 
