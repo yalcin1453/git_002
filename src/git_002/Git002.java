b10fe56a4878f25e51855168c462ee6694f3ac2a
Fatih <yalcinfatihtr@gmail.com>
2020-08-09 11:11:38 -0400

    secon commit

--- src/git_002/Git002.java
+++ src/git_002/Git002.java
@@ -5,6 +5,7 @@ public class Git002 {
 	public static void main(String[] args) {
 	
 		System.out.println("this is the first changes from local master");
+		System.out.println("this is second commit from local master");
 
 	}
 
